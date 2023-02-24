a469701e8adc65961e12f6411bd8d6d82d8affee
mrmao <wwu710632@gmail.com>
2023-02-24 18:20:41 +0800

    feat: simplify parser logic

--- packages/swag-ts/src/parser/index.ts
+++ packages/swag-ts/src/parser/index.ts
@@ -1,6 +1,6 @@
 import camelCase from 'lodash/camelCase'
 import type { ApiPipeline, StatementFunction, StatementInterface, StatementTypeAlias } from 'apipgen'
-import type { OpenAPISpecificationV2, Schema } from 'openapi-specification-types'
+import type { Definitions, OpenAPISpecificationV2, Paths, Schema } from 'openapi-specification-types'
 import { getFunctionOptions, getPropertieType, traversePaths } from './utils'
 import { varName } from './utils/format'
 import { literalFieldsToString } from './utils/other'
@@ -20,96 +20,16 @@ export function parser(configRead: ApiPipeline.ConfigRead) {
   const interfaces: StatementInterface[] = []
   const functions: StatementFunction[] = []
 
-  function transformPaths() {
-    traversePaths(source.paths, (config) => {
-      const { method, path, options: meta } = config
-      /**
-       * function params/function options/function use interfaces
-       */
-      const { parameters, interfaces: interfaceUses, options } = getFunctionOptions(config)
-
-      interfaces.push(...interfaceUses)
-
-      /**
-       * function comments
-       */
-      const comments = [
-        meta.summary && `@summary ${meta.summary}`,
-        meta.description && `@description ${meta.description}`,
-        `@method ${method}`,
-        meta.tags && `@tags ${meta.tags.join(' | ') || '-'}`,
-        meta.consumes && `@consumes ${meta.consumes.join('; ') || '-'}`,
-      ]
-      /**
-       * function name
-       */
-      const name = camelCase(`${method}/${path}`)
-      const url = `${path.replace(/({)/g, '${paths?.')}`
-
-      /**
-       * response type
-       */
-      const responseType = meta.responses['200'] ? getPropertieType(meta.responses['200']) : 'void'
-      const prefixType = configRead.config.output?.type === false ? '' : 'OpenAPITypes.'
-      const genericType = `${prefixType}Response<${spliceTypeSpace(responseType)}>`
-
-      functions.push({
-        export: true,
-        name,
-        description: comments.filter(Boolean),
-        parameters,
-        body: [
-          url.includes('$') ? `const url = \`${url}\`;` : `const url = "${url}"`,
-          `http.request<${genericType}>>({ ${literalFieldsToString(options)} })`,
-        ],
-      })
-    })
-  }
-
-  function transformDefinitions() {
-    for (const [name, definition] of Object.entries(source.definitions)) {
-      const { properties = {} } = definition
-
-      interfaces.push({
-        export: true,
-        name: varName(name),
-        properties: Object.keys(properties).map(name => defToFields(name, properties[name])),
-      })
-
-      function defToFields(name: string, propertie: Schema) {
-        propertie.required = definition?.required?.some(v => v === name)
-        if (propertie.description)
-          propertie.description = `@description ${propertie.description}`
-        return {
-          name,
-          type: getPropertieType(propertie),
-          description: propertie.description,
-          required: propertie.required,
-        }
-      }
-    }
-  }
-
-  function transformNameSpace() {
-    for (const iterator of functions) {
-      for (const parameter of iterator.parameters || []) {
-        if (parameter.type)
-          parameter.type = spliceTypeSpace(parameter.type)
-      }
-    }
-  }
-
-  function spliceTypeSpace(name: string) {
-    const isRenderType = configRead.config.output?.type !== false
-    const isSomeType = interfaces.map(v => v.name).includes(name.replace('[]', ''))
-    if (isRenderType && isSomeType)
-      return `OpenAPITypes.${name}`
-    return name
-  }
-
-  transformPaths()
-  transformDefinitions()
-  transformNameSpace()
+  pathsPuFunctions(source.paths, {
+    configRead,
+    functions,
+    interfaces,
+  })
+  defPuInterfaces(source.definitions, {
+    configRead,
+    functions,
+    interfaces,
+  })
 
   configRead.graphs.comments = comments
   configRead.graphs.functions = functions
@@ -117,4 +37,93 @@ export function parser(configRead: ApiPipeline.ConfigRead) {
   configRead.graphs.interfaces = interfaces
 
   return configRead
+}
+
+interface TransformOptions {
+  configRead: ApiPipeline.ConfigRead
+  interfaces: StatementInterface[]
+  functions: StatementFunction[]
+}
+
+function pathsPuFunctions(paths: Paths, { configRead, functions, interfaces }: TransformOptions) {
+  traversePaths(paths, (config) => {
+    const { method, path, options: meta } = config
+    /**
+     * function params/function options/function use interfaces
+     */
+    const { parameters, interfaces: interfaceUses, options } = getFunctionOptions(config)
+
+    interfaces.push(...interfaceUses)
+
+    /**
+     * function comments
+     */
+    const comments = [
+      meta.summary && `@summary ${meta.summary}`,
+      meta.description && `@description ${meta.description}`,
+      `@method ${method}`,
+      meta.tags && `@tags ${meta.tags.join(' | ') || '-'}`,
+      meta.consumes && `@consumes ${meta.consumes.join('; ') || '-'}`,
+    ]
+    /**
+     * function name
+     */
+    const name = camelCase(`${method}/${path}`)
+    const url = `${path.replace(/({)/g, '${paths?.')}`
+
+    /**
+     * response type
+     */
+    const responseType = meta.responses['200'] ? getPropertieType(meta.responses['200']) : 'void'
+    const prefixType = configRead.config.output?.type === false ? '' : 'OpenAPITypes.'
+    const genericType = `${prefixType}Response<${spliceTypeSpace(responseType)}>`
+
+    function spliceTypeSpace(name: string) {
+      const isRenderType = configRead.config.output?.type !== false
+      const isSomeType = interfaces.map(v => v.name).includes(name.replace('[]', ''))
+      if (isRenderType && isSomeType)
+        return `OpenAPITypes.${name}`
+      return name
+    }
+
+    for (const parameter of parameters || []) {
+      if (parameter.type)
+        parameter.type = spliceTypeSpace(parameter.type)
+    }
+
+    functions.push({
+      export: true,
+      name,
+      description: comments.filter(Boolean),
+      parameters,
+      body: [
+        url.includes('$') ? `const url = \`${url}\`;` : `const url = "${url}"`,
+        `http.request<${genericType}>>({ ${literalFieldsToString(options)} })`,
+      ],
+    })
+  })
+}
+
+function defPuInterfaces(definitions: Definitions, { interfaces }: TransformOptions) {
+  for (const [name, definition] of Object.entries(definitions)) {
+    const { properties = {} } = definition
+
+    interfaces.push({
+      export: true,
+      name: varName(name),
+      properties: Object.keys(properties).map(name => defToFields(name, properties[name])),
+    })
+
+    function defToFields(name: string, propertie: Schema) {
+      propertie.required = definition?.required?.some(v => v === name)
+      if (propertie.description)
+        propertie.description = `@description ${propertie.description}`
+      return {
+        name,
+        type: getPropertieType(propertie),
+        description: propertie.description,
+        required: propertie.required,
+      }
+    }
+  }
 }
