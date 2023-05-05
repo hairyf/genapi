e96b006ff52b19b4db9a5f656a4fc0852bad09ae
mrmao <wwu710632@gmail.com>
2023-05-05 14:34:18 +0800

    fix: publish access public error

--- packages/@genapi/config/src/types/config.ts
+++ packages/@genapi/config/src/types/config.ts
@@ -73,7 +73,6 @@ namespace ApiPipeline {
   }
 
   export interface Graphs {
-
     /**
      * all comments
      */
--- packages/@tests/bin/index.js
+++ packages/@tests/bin/index.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 
-const path = require('path')
-
 require('esbuild-register')
 require('@genapi/cli/src/bin/index.ts')
