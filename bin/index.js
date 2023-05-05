29936a884ef88a8c6e7f6b90030308f5823fc89a
mrmao <wwu710632@gmail.com>
2023-05-05 13:27:31 +0800

    chore: clear excess scripts

--- bin/index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-require('esbuild-register')
-require('../packages/@genapi/cli/src/bin/index.ts')
--- ptsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'ptsup'
-
-const config = defineConfig({
-  assets: [
-    './helper/typings',
-  ],
-})
-
-export default config
