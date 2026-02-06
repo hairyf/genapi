d5cff3e6863b6df3ea16be32583c549024b2d6e1
hairyf <wwu710632@gmail.com>
2026-02-06 20:24:45 +0800

    feat: add documentation and configuration for new docs package, enhance ESLint settings, and update pnpm workspace

--- eslint.config.js
+++ eslint.config.js
@@ -5,13 +5,14 @@ export default antfu(
   {
     type: 'lib',
     pnpm: true,
-    ignores: ['packages/**/*.md'],
+    ignores: ['packages/**/*.md', 'docs/**/*.md'],
   },
   {
     rules: {
       'no-template-curly-in-string': 'off',
       'ts/explicit-function-return-type': 'off',
       'ts/no-namespace': 'off',
+      'pnpm/yaml-enforce-settings': 'off',
     },
   },
 )
